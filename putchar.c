1d8328e1f6f12e436d89509b718c57013a00b9c1
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-03-21 20:31:01 -0300

    Update putchar.c - media para 6 digitios

--- putchar.c
+++ putchar.c
@@ -9,17 +9,31 @@ void put2dig(int num){
     putdig(num%10);
 }
 
-put3dig(int num){
-    int d1, d2, d3;
+void put3dig(int num){
     put2dig(num/10);
+    putdig(num%10);
+}
+
+void put4dig(int num){
+    put3dig(num/10);
+    putdig(num%10);
+}
+
+void put5dig(int num){
+    put4dig(num/10);
+    putdig(num%10);
+}
+
+void put6dig(int num){
+    put5dig(num/10);
     putdig(num%10);
 }
 
 int main(){
     int n1, n2, media;
-    n1=123;
-    n2=271;
+    n1=123457;
+    n2=271457;
 
     media=(n1+n2)/2;
-    put3dig(media);
+    put6dig(media);
 }
