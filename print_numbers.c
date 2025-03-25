2a1e1db8e4f1fce71ecf039cf645d837fcd1438e
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-03-25 15:36:22 -0300

    Update print_numbers.c

--- print_numbers.c
+++ print_numbers.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 
 void putdig(int dig){
+	if(dig<0){
+		putchar('-');
+		dig = -dig;
+	}
 	if(dig>9){
-	putdig(dig/10);
+		putdig(dig/10);
 	}
 	putchar('0'+dig%10);
 }
 
 int main(){
-	int n1, n2;
+	int n1, n2, n3;
 	n1=123456789;
 	n2=5;
 	putdig(n1);
 	putchar('\n');
 	putdig(n2);
+	putchar('\n');
+	putdig(n3);
 	putchar('\n');
 }
 
