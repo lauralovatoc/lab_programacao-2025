32aacbc0e2f2fa51dfac2864e8912b5006328266
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-03-25 15:23:55 -0300

    Update print_numbers.c

--- print_numbers.c
+++ print_numbers.c
@@ -7,49 +7,13 @@ void putdig(int dig){
 	putchar('0'+dig%10);
 }
 
-void put2dig(int num){
-	if(num>9){
-	putdig(num/10);
-	}
-	putdig(num%10);
-}
-
-void put3dig(int num){
-	if(num>9){
-	put2dig(num/10);
-	}
-	putdig(num%10);
-}
-
-void put4dig(int num){
-	if(num>9){
-	put3dig(num/10);
-	}
-	putdig(num%10);
-}
-
-void put5dig(int num){
-	if(num>9){
-	put4dig(num/10);
-	}
-	putdig(num%10);
-}
-
-void put6dig(int num){
-	if(num>9){
-	put5dig(num/10);
-	}
-
-	putdig(num%10);
-}
-
 int main(){
 	int n1, n2;
 	n1=123456789;
 	n2=5;
-	put6dig(n1);
+	putdig(n1);
 	putchar('\n');
-	put6dig(n2);
+	putdig(n2);
 	putchar('\n');
 }
 
