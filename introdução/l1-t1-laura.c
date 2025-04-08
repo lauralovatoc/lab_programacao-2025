e053f74d3813fa3237987eb2fbf1bd31fe4297d5
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-04-08 16:06:58 -0300

    Update l1-t1-laura.c

--- introdução/l1-t1-laura.c
+++ introdução/l1-t1-laura.c
@@ -4,9 +4,18 @@
 #include <stdio.h>
 
 int main(){
-    for(int valor=32;valor<=126;valor++){
-        printf("Codigo: %d   Caractere: ",valor);
-        putchar(valor);
+    int hexadecimal = 0x20;
+    int binario = 0b100000;
+    int octal = 0o40;
+    int decimal =32;
+    //todos equivalem a 32
+    while(decimal<=126){
+        printf("Codigo: %d  Hexadecimal: %h  Binario: %b  Octal: %o  Caractere: ",decimal, hexadecimal, binario, octal);
+        putchar(decimal);
         putchar('\n');
+        decimal++;
+        hexadecimal++;
+        octal++;
+        decimal++;
     }
 }
