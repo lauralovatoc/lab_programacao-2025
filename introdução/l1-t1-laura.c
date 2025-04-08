bd643ca0c877fbc91bd3a8e317bd2ed7edf30953
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-04-08 19:53:31 -0300

    Update l1-t1-laura.c

--- introdução/l1-t1-laura.c
+++ introdução/l1-t1-laura.c
@@ -1,21 +1,12 @@
-//imprimir tabela ASCII
+//imprimir tabela ASCII e bases numéricas de 32 a 126
 //aluna: Laura Lovato Camponogara, turma CC1
 
 #include <stdio.h>
 
 int main(){
-    int hexadecimal = 0x20;
-    int binario = 0b100000;
-    int octal = 0o40;
-    int decimal =32;
-    //todos equivalem a 32
-    while(decimal<=126){
-        printf("Decimal: %d  Hexadecimal: %x  Binario: %b  Octal: %o  Caractere: ",decimal, hexadecimal, binario, octal);
-        putchar(decimal);
+    for(int valor=32;valor<=126;valor++){
+         printf("Decimal: %d  Hexadecimal: %x  Binario: %b  Octal: %o  Caractere: ",valor, valor, valor, valor);
+        putchar(valor);
         putchar('\n');
-        decimal++;
-        hexadecimal++;
-        octal++;
-        decimal++;
     }
 }
