ca65d04a0221581710c50a59fa617ad2e96da141
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-04-10 14:54:31 -0300

    Create exercicio-2.c

---
+++ lacos-de-repeticao/exercicio-2.c
@@ -0,0 +1,11 @@
+//Faça um programa que imprime os números de 20 a 29, separados por espaços, usando um comando de repetição.
+#include <stdio.h>
+
+int main(){
+    int num=20;
+    
+    while(num<=29){
+        printf("%d ", num);
+        num++;
+    }
+}
