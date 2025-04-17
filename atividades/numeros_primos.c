32de7305004c9824270e0b79bd629e6b3dba9cd7
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-04-17 14:57:19 -0300

    Add files via upload

---
+++ atividades/numeros_primos.c
@@ -0,0 +1,37 @@
+//todos os numeros primos menores que 10.000
+#include<stdio.h>
+#include<stdbool.h>
+
+bool verifica_primos(int num){
+    int cont=1, sum=0;
+
+    while(cont<num){
+        if(num%cont==0){
+            sum += cont;
+        }
+        cont++;
+    }
+
+    if(sum==1){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+int main(){
+    int num=1;
+    bool num_primo;
+
+    printf("Numeros primos:\n\n");
+
+    while(num<=10000){
+        num_primo = verifica_primos(num);
+        if(num_primo == true){
+            printf("%d ", num);
+            num_primo = false;
+        }
+        num++;
+    }
+
+}
